023ba91791427647d1a4c344176a366e380ba0c7
SuperDelphi <superdelphi@pm.me>
2024-05-01 12:09:14 +0200

    Added scopes handling + event emitting

--- src/ts/client/grapesjs/keybinds.ts
+++ src/ts/client/grapesjs/keybinds.ts
@@ -1,4 +1,5 @@
 import { Editor } from 'grapesjs'
+import { cmdAddPage } from './page-panel'
 
 /**
  * Represents key modifiers.
@@ -13,9 +14,38 @@ export enum KeyModifiers {
 /**
  * Represents the trigger scope of a keybinding.
  */
-export enum TriggerScope {
-
+export type TriggerScope = {
+  name: string,
+  condition: (editor: Editor) => boolean
 }
+
+const TriggerScopes = {
+  GLOBAL: {
+    name: 'Global',
+    description: 'Applies everywhere in the editor.',
+    condition: editor => true
+  },
+  GLOBAL_NO_TEXT_EDIT: {
+    name: 'Global (Excepted Text Edition)',
+    description: 'Applies everywhere but while editing text.',
+    condition: editor => true // TODO: Replace
+  },
+  TEXT_EDIT: {
+    name: 'Text Edition',
+    description: 'Applies while editing text.',
+    condition: editor => true // TODO: Replace
+  }
+}
+
+/**
+ * The key separator.
+ */
+const keySep: string = '+'
+
+/**
+ * The event namespace for this plugin.
+ */
+const eventName: string = 'keybind'
 
 /**
  * A key (optionally associated with modifiers)
@@ -25,37 +55,54 @@ export enum TriggerScope {
 export type KeyBind = {
   key: string,
   modifiers: string,
-  command: string
+  command: string,
+  scope: TriggerScope,
+  preventDefault: boolean,
 }
 
 /**
  * Contains all the registered keybindings.
  */
-export const keyBindsMap: Map<string, KeyBind> = new Map()
+export const keybindsMap: Map<string, KeyBind> = new Map()
 
 /**
  * Registers a keybinding.
- * Syntax for ``keys``:
+ * Syntax for ``keys`` (if the separator is ``+``):
  * ```
- * <modifier1> [<modifier2> [<modifier3>]] <key>
+ * <modifier1>+[<modifier2>+[<modifier3>]]+<key>
  * ```
- * Examples: ``ctrl j``, ``p``, ``ctrl alt shift t``, etc.
+ * Examples: ``ctrl+j``, ``p``, ``ctrl+alt+shift+t``, etc.
  */
-export function setKeyBind(keys: string, command: string): KeyBind {
-  const splitKeys = keys.toLowerCase().split(' ')
+export function setKeyBind(editor: Editor, keys: string, command: string, scope: TriggerScope = TriggerScopes.GLOBAL, preventDefault: boolean = true): KeyBind {
+  const splitKeys = keys.toLowerCase().split(keySep)
 
   if (splitKeys.length > 0) {
     const key: string = splitKeys.pop()
     const modifiers: string = formattedModifiers(splitKeys)
 
-    const keyBind: KeyBind = {key, modifiers, command}
-    keyBindsMap.set(modifiers + ' ' + key, keyBind)
-    console.log(keyBind)
+    const keyBind: KeyBind = {key, modifiers, command, scope, preventDefault}
+    const entryKey: string = modifiers + keySep + key
+
+    if (!keybindsMap.has(entryKey)) {
+      editor.trigger(eventName + ':add')
+    } else {
+      editor.trigger(eventName + ':update')
+    }
+
+    keybindsMap.set(entryKey, keyBind)
 
     return keyBind
   } else {
     throw new Error('The keybinding \'' + keys + '\' is invalid.')
   }
+}
+
+export function removeKeyBind(editor, keys): boolean {
+  const wasPresent: boolean = keybindsMap.delete(keys)
+
+  if (wasPresent) editor.trigger(eventName + ':remove')
+
+  return wasPresent
 }
 
 /**
@@ -85,7 +132,7 @@ function formattedModifiers(modifiers: string[]): string {
     }
   })
 
-  return ordered.join(' ')
+  return ordered.join(keySep)
 }
 
 function formattedModifiersFrom(event: KeyboardEvent): string {
@@ -96,24 +143,27 @@ function formattedModifiersFrom(event: KeyboardEvent): string {
   event.altKey && modifiers.push(KeyModifiers.AltKey)
   event.shiftKey && modifiers.push(KeyModifiers.ShiftKey)
 
-  return modifiers.join(' ')
+  return modifiers.join(keySep)
 }
 
 /**
  * The function that initializes the keybindings' plugin.
+ * It starts watching for registered keybindings.
  * @param editor The editor.
  */
 export function keybindsPlugin(editor: Editor) {
-  // TODO: Remove tests
-  setKeyBind('ctrl d', 'core:component-delete')
+  console.log(setKeyBind(editor, 'shift+n', cmdAddPage, TriggerScopes.GLOBAL_NO_TEXT_EDIT))
 
   window.addEventListener('keydown', event => {
     const modifiers: string = formattedModifiersFrom(event)
 
-    keyBindsMap.forEach(keybind => {
-      console.log(event.key + '(' + modifiers + ')')
+    keybindsMap.forEach(keybind => {
+      if (keybind.scope.condition(editor) && keybind.modifiers === modifiers && keybind.key === event.key.toLowerCase()) {
+        const keyId: string = keybind.modifiers + keySep + keybind.key
 
-      if (keybind.modifiers === modifiers && keybind.key === event.key) {
+        if (keybind.preventDefault) event.preventDefault()
+        editor.trigger(eventName + ':emit', keybind, event)
+        editor.trigger(eventName + ':emit:' + keyId, keybind, event)
         editor.runCommand(keybind.command)
       }
     })
